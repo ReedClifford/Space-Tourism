142fedc6da360b888b556d3337cf05e953b6b62f
Shichi <hakubaaaa@gmail.com>
2022-12-10 11:07:22 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      barlow: ["Barlow", "sans-serif"],
+      bellefair: ["Bellefair", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
